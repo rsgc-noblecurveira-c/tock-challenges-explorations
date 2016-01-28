4cf09d3ad40ae3322070254b2d6563784822fe74
rsgc-noblecurveira-c <student@MacBook-Pro-Noble-Curviera.local>
2016-01-28 10:52:17 -0500

    I figured it out but did not have enough time to implement

--- Tock-Challenges/Day1-2.playground/Contents.swift
+++ Tock-Challenges/Day1-2.playground/Contents.swift
@@ -2,4 +2,36 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+let input: String = "How are you :-) doing :-( today :-)?"
+var characters: [String] = []
+
+let delimiters = NSCharacterSet(charactersInString: ", \n")
+let words = input.componentsSeparatedByCharactersInSet(delimiters)
+
+var happyCount = 0
+var sadCount = 0
+var wordCount = 0
+let happyFace = ":-)"
+
+//for (var i = 0; i < words.count; i++)
+//{
+//    if (String(words[i]) == happyFace)
+//    {
+//        happyCount++
+//    } else if (String(words[i]) == "today")
+//    {
+//        sadCount++
+//    } else {
+//        wordCount++
+//    }
+//}
+
+for character in input.characters
+{
+characters += [String(character)]
+    
+    if character == ":"
+    {
+        print("i found it")
+    }
+}
