5a8871f347aec8aabab01b202094e33c26f835c3
rsgc-noblecurveira-c <student@MacBook-Pro-Noble-Curviera.local>
2016-01-28 10:16:51 -0500

    Completed using if statements

--- Tock-Challenges/Day1-1.playground/Contents.swift
+++ Tock-Challenges/Day1-1.playground/Contents.swift
@@ -2,4 +2,28 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+let inputMonth: Int = 2
+let inputDay: Int = 18
+
+let specialMonth: Int = 2
+let specialDay: Int = 18
+
+if (inputMonth == specialMonth)
+{
+    if (inputDay == specialDay)
+    {
+        print("Special")
+    } else if (inputDay < specialDay)
+    {
+        print("Before")
+    } else
+    {
+        print("After")
+    }
+} else if (inputMonth < specialMonth)
+{
+    print("Before")
+} else
+{
+    print("After")
+}
