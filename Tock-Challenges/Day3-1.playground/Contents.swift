5a3672634c87e9f1b62a9a97bfd3690f6059367f
rsgc-noblecurveira-c <student@MacBook-Pro-Noble-Curviera.local>
2016-02-03 10:36:55 -0500

    Fix, so that you can input any 13 digit number as a string

--- Tock-Challenges/Day3-1.playground/Contents.swift
+++ Tock-Challenges/Day3-1.playground/Contents.swift
@@ -2,26 +2,27 @@
 
 import Cocoa
 
-var inputThing: [Int] = [9, 7, 8, 0, 9, 2, 1, 4, 1, 8]
-var digit11 = 0
-var digit12 = 5
-var digit13 = 2
+var inputThing: [String] = []
 var total = 0
+var inputNumber = "3210423897234"
 
 
-inputThing += [digit11]
-inputThing += [digit12]
-inputThing += [digit13]
+for character in inputNumber.unicodeScalars
+{
+    inputThing += [String(character)]
+}
+
+inputThing
 
 for (var i = 0; i < 13; i++)
 {
     if(i%2 == 0)
     {
-        inputThing[i] = inputThing[i] * 1
+        inputThing[i] = String(Int(inputThing[i])! * 1)
     } else {
-        inputThing[i] = inputThing[i] * 3
+        inputThing[i] = String(Int(inputThing[i])! * 3)
     }
-    total += Int(inputThing[i])
+    total += Int(inputThing[i])!
 }
 
 print(total)
